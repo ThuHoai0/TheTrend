4f42aa68c8b75db46546f379d748ee30dbe397e5
ThuHoai0 <Maithihoaithu0@gmail.com>
2024-12-03 03:27:16 +0700

    ok

--- quenmatkhau.php
+++ quenmatkhau.php
@@ -19,16 +19,7 @@
             color: #333;
         }
 
-        /* Banner */
-        .banner {
-            text-align: center;
-            background-color: rgba(0, 123, 255, 0.8);
-            color: white;
-            padding: 20px;
-            border-radius: 8px 8px 0 0;
-            margin-bottom: -5px;
-            box-shadow: 0px 4px 10px rgba(0, 0, 0, 0.1);
-        }
+
 
         .banner h1 {
             font-size: 28px;
@@ -70,12 +61,9 @@
             margin-bottom: 0;
         }
 
-        /* Gender and Date Inputs */
-        .form-check-input {
-            margin-right: 8px;
-        }
 
-        .row .form-select {
+
+        .row  {
             font-size: 14px;
         }
 
@@ -94,17 +82,6 @@
             transform: scale(1.03);
         }
 
-        /* Link Styling */
-        .btn-link {
-            font-size: 14px;
-            color: #007bff;
-            text-decoration: none;
-            padding-left: 10px;
-        }
-
-        .btn-link:hover {
-            text-decoration: underline;
-        }
     </style>
 </head>
 <body>
