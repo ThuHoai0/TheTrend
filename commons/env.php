6be3ef5283a0f2005fb7804062462f3416eed94e
Thu Hoai <maithihoaithu0@gmail.com>
2024-12-07 14:19:28 +0700

    Update env.php

--- commons/env.php
+++ commons/env.php
@@ -1,14 +1,1 @@
-<?php 
 
-// Biến môi trường, dùng chung toàn hệ thống
-// Khai báo dưới dạng HẰNG SỐ để không phải dùng $GLOBALS
-
-define('BASE_URL'   , 'http://localhost/TheTrend/');
-
-define('DB_HOST'    , 'localhost');
-define('DB_PORT'    , 3306);
-define('DB_NAME'    , 'thetrend_duan1');  // Tên database
-define('DB_USERNAME', 'root');
-define('DB_PASSWORD', '');
-
-define('PATH_ROOT'    , __DIR__ . '/../');
